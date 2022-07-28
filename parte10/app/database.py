fe811c1941a2eb7b231d1e99164643668344e97b
Romulo Menezes <romulojnr@gmail.com>
2022-07-27 21:04:47 -0300

    actions: mysql service

--- parte10/app/database.py
+++ parte10/app/database.py
@@ -7,15 +7,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.settings import settings
 
-SQLALCHEMY_DATABASE_URL = settings.database_sqlite
+# SQLALCHEMY_DATABASE_URL = settings.database_sqlite
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-# SQLALCHEMY_DATABASE_URL = (
-#     f"mysql+pymysql://"
-#     f"{settings.database_username}:{settings.database_password}@"
-#     f"{settings.database_hostname}:{settings.database_port}/"
-#     f"{settings.database_name}"
-# )
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://"
+    f"{settings.database_username}:{settings.database_password}@"
+    f"{settings.database_hostname}:{settings.database_port}/"
+    f"{settings.database_name}"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
