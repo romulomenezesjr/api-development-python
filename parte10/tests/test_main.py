6307f86ce973ec3a1ffc13c19237098af1be673a
Romulo Menezes <romulojnr@gmail.com>
2022-07-30 08:23:40 -0300

    actions: deploying to heroku

--- parte10/tests/test_main.py
+++ parte10/tests/test_main.py
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "hello world"}
+    assert response.json() == {"message": "hello heroku"}
